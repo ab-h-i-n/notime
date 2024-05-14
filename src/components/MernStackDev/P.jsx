5fbc413136e6e27d62d89eeccc30ccbe4cfee099
ab-h-i-n <abhinshelly@gmail.com>
2024-05-14 09:36:40 +0530

    chore: Add viewport once property to motion.p component

--- src/components/MernStackDev/P.jsx
+++ src/components/MernStackDev/P.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const P = ({ children, className }) => {
-  return <motion.p initial={{opacity : 0, y : 20}} whileInView={{ opacity : 1 , y : 0 }} transition={{ delay : 0.2 }} className={`text-slate-300 md:text-xl lg:leading-[1.7rem] ${className}`}>{children}</motion.p>;
+  return <motion.p initial={{opacity : 0, y : 20}} whileInView={{ opacity : 1 , y : 0 }} transition={{ delay : 0.2 }} viewport={{ once : true }} className={`text-slate-300 md:text-xl lg:leading-[1.7rem] ${className}`}>{children}</motion.p>;
 };
 
 export default P;
