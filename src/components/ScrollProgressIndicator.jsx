f0f5f22265640b14dece86f2276cb95f7c7a07fa
ab-h-i-n <abhinshelly@gmail.com>
2024-05-14 12:56:23 +0530

    chore: Update ScrollProgressIndicator component with framer-motion useTransform hook

--- src/components/ScrollProgressIndicator.jsx
+++ src/components/ScrollProgressIndicator.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
-import { motion, useSpring, useScroll } from "framer-motion";
+import { motion, useSpring, useScroll, useTransform } from "framer-motion";
 
 const ScrollProgressIndicator = () => {
-  useEffect(() => {
+
+
     const { scrollYProgress } = useScroll();
     const scaleX = useSpring(scrollYProgress);
-  }, []);
 
   return (
     <>
       {/* scroll progress indicator  */}
       <motion.div
-        style={{ scaleX: scaleX }}
-        className={`w-screen origin-left fixed top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-yellow-500 z-[100]`}
+        style={{ scaleX: scaleX}}
+        className={`w-screen origin-left rounded-full fixed top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-yellow-500 z-[100]`}
       ></motion.div>
     </>
   );
