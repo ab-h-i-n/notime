28326d34f5e393ca9630431f924d814f3db78590
ab-h-i-n <abhinshelly@gmail.com>
2024-05-19 20:33:10 +0530

    level indicator added

--- src/components/MernStackDev/LevelCard.jsx
+++ src/components/MernStackDev/LevelCard.jsx
@@ -1,51 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useRef } from "react";
 import P from "../common/P";
 import GradientText from "../common/GradientText";
-import { client } from "../../utils/sanity/client"
-import imageUrlBuilder from '@sanity/image-url'
+import { client } from "../../utils/sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import H1 from "../common/H1";
+import { motion } from "framer-motion";
 
+const LevelCard = ({ level, gradient }) => {
 
-
-const LevelCard = ({ level , gradient }) => {
   const builder = imageUrlBuilder(client);
+  const containerRef = useRef();
 
   return (
-    <div className="relative">
+    <div ref={containerRef} className="w-full max-w-[600px] bg-[#101822] pt-20 pb-16 px-5 rounded-2xl relative border-[0.5px] border-slate-800">
+      
+      {/* level indicator  */}
+      <motion.div  className="opacity-50 w-min bg-[#070910] flex justify-end text-lg xl:text-xl font-black absolute top-10 right-0 p-2 md:pl-3 md:pr-4 xl:pr-6 text-nowrap rounded-l-xl z-[2]">{level?.title}</motion.div>
 
       {/* image  */}
-      <div className="absolute h-[200px] aspect-square left-5 -top-20">
-        <img src={level?.icon && builder.image(level.icon).url()} className="w-full h-auto object-cover" alt="icon" />
-      </div>
-
-      {/* details  */}
-      <div className="bg-[#101822] pt-20 pb-16 px-5 rounded-md ">
-        {/* heading  */}
-        <h1 className={"font-bold text-4xl py-5 w-[250px]"}>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.2, delay: 0.5 }}
+        className="absolute h-[200px] aspect-square left-5 xl:left-10 -top-20"
+      >
+        <img
+          src={level?.icon && builder.image(level.icon).url()}
+          className="w-full h-auto object-cover"
+          alt="icon"
+        />
+      </motion.div>
+      {/* heading  */}
+      <H1>
+        <div
+          className={
+            "font-bold text-3xl md:text-4xl py-5 w-[300px] md:w-[350px]"
+          }
+        >
           {level?.level_title}
-        </h1>
-        {/* content  */}
-        <div className="grid gap-5">
-          {level?.main_contents?.map((main, index) => (
-            <div className="grid gap-3">
-              <div className="flex  items-center gap-3">
-                <GradientText gradient={gradient} className={`font-extrabold text-2xl`}>
-                  {index + 1}
-                </GradientText>
-                <P className="font-semibold text-white">{main?.title}</P>
-              </div>
-              <div>
-                {main?.content?.map((c, index) => (
-                  <div className="flex pl-10 text-sm py-[.5px]">
-                    <span className="text-slate-400 mr-2">#</span>
-                    <P key={index}>
-                      {c}
-                    </P>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
         </div>
+      </H1>
+      {/* content  */}
+      <div className="grid gap-5 relative z-10">
+        {level?.main_contents?.map((main, index) => (
+          <div className="grid gap-3">
+            <div className="flex  items-center gap-3">
+              <GradientText
+                gradient={gradient}
+                className={`font-extrabold text-2xl`}
+              >
+                {index + 1}
+              </GradientText>
+              <P className="font-semibold text-[#ffffffe9] max-w-[300px]">{main?.title}</P>
+            </div>
+            <div>
+              {main?.content?.map((c, index) => (
+                <div className="flex pl-10 text-sm py-[.5px]">
+                  <span className="text-slate-400 mr-2 ">#</span>
+                  <P key={index}>{c}</P>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/MernStackDev/WhatWillYouLearn.jsx
+++ src/components/MernStackDev/WhatWillYouLearn.jsx
@@ -3,6 +3,7 @@ import H1 from "../common/H1";
 import { client } from "../../utils/sanity/client";
 import LevelCard from "./LevelCard";
 
+
 const WhatWillYouLearn = () => {
   const [levels, setLevels] = useState();
 
@@ -34,23 +35,24 @@ const WhatWillYouLearn = () => {
     'from-sky-500 to-violet-500',
     'from-rose-500 to-amber-500',
     'from-blue-500 to-green-500',
+    'from-violet-500 to-sky-500',
   ];
 
   return (
-    <div className="grid gap-10 md:gap-10 my-10 max-w-2xl">
+    <div className="grid gap-10 md:gap-10 my-10 max-w-6xl overflow-y-hidden">
       {/* heading  */}
-      <div className="flex items-center gap-3 mb-16">
+      <H1 className="flex items-center justify-center gap-3 mb-16">
         {/* arrow left  */}
         <img src="/arrow-down.svg" alt="arrow" className="w-12" />
         {/* text  */}
         <H1 className={"text-nowrap"}>See what you'll learn</H1>
         {/* arrow right  */}
         <img src="/arrow-down.svg" alt="arrow" className="w-12" />
-      </div>
+      </H1>
 
       {/* levels */}
 
-      <div className="grid gap-20">
+      <div className="grid gap-20 place-items-center lg:grid-cols-2">
         {levels?.map((level, index) => (
           <>
             <LevelCard key={level?.title} level={level} gradient={gradientsForLevels[index % gradientsForLevels.length]} />
